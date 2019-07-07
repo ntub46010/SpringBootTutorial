5533429bd789cdb2a1ecbdb558ba7c99e5dc317b
Vincent <eric61011@gmail.com>
2019-07-07 18:36:18 +0800

    Upload chapter 10

--- src/test/java/com/vincent/demo/ProductTest.java
+++ src/test/java/com/vincent/demo/ProductTest.java
@@ -1,17 +1,27 @@
 package com.vincent.demo;
 
+import com.vincent.demo.entity.Product;
+import com.vincent.demo.repository.ProductRepository;
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,28 +33,123 @@ public class ProductTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    private HttpHeaders httpHeaders;
+
+    @Before
+    public void init() {
+        httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json");
+    }
+
+    @After
+    public void clear() {
+        productRepository.deleteAll();
+    }
+
     @Test
     public void testCreateProduct() throws Exception {
         JSONObject request = new JSONObject();
         request.put("name", "Harry Potter");
         request.put("price", 450);
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Content-Type", "application/json");
-
-        RequestBuilder requestBuilder =
-                MockMvcRequestBuilders.post("/products")
-                        .headers(httpHeaders)
-                        .content(request.toString());
-
-        mockMvc.perform(requestBuilder)
+        MvcResult result = mockMvc.perform(post("/products")
+                .headers(httpHeaders)
+                .content(request.toString()))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").hasJsonPath())
+                .andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+        JSONObject resBody = new JSONObject(response.getContentAsString());
+        String productId = resBody.getString("id");
+
+        assertNotNull(productId);
+        assertEquals(request.getString("name"), resBody.getString("name"));
+        assertEquals(request.getString("price"), resBody.getString("price"));
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+        assertEquals("application/json;charset=UTF-8", response.getContentType());
+        assertEquals(result.getRequest().getRequestURL() + "/" + productId,
+                response.getHeader("Location"));
+
+        assertEquals(1, productRepository.findAll().size());
+        assertTrue(productRepository.findById(productId).isPresent());
+    }
+
+    @Test
+    public void testGetProduct() throws Exception {
+        Product product = createProduct("Economics", 450);
+        productRepository.insert(product);
+
+        mockMvc.perform(get("/products/" + product.getId())
+                .headers(httpHeaders))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(product.getId()))
+                .andExpect(jsonPath("$.name").value(product.getName()))
+                .andExpect(jsonPath("$.price").value(product.getPrice()));
+    }
+
+    @Test
+    public void testReplaceProduct() throws Exception {
+        Product product = createProduct("Economics", 450);
+        productRepository.insert(product);
+
+        JSONObject request = new JSONObject();
+        request.put("name", "Macroeconomics");
+        request.put("price", 550);
+
+        mockMvc.perform(put("/products/" + product.getId())
+                .headers(httpHeaders)
+                .content(request.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(product.getId()))
                 .andExpect(jsonPath("$.name").value(request.getString("name")))
-                .andExpect(jsonPath("$.price").value(request.getInt("price")))
-                .andExpect(header().exists("Location"))
-                .andExpect(header().string("Content-Type", "application/json;charset=UTF-8"));
+                .andExpect(jsonPath("$.price").value(request.getInt("price")));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testDeleteProduct() throws Exception {
+        Product product = createProduct("Economics", 450);
+        productRepository.insert(product);
+
+        mockMvc.perform(delete("/products/" + product.getId())
+                .headers(httpHeaders))
+                .andExpect(status().isNoContent());
+
+        productRepository.findById(product.getId())
+                .orElseThrow(RuntimeException::new);
+    }
+
+    @Test
+    public void testSearchProductsSortByPriceAsc() throws Exception {
+        Product p1 = createProduct("Operation Management", 350);
+        Product p2 = createProduct("Marketing Management", 200);
+        Product p3 = createProduct("Human Resource Management", 420);
+        Product p4 = createProduct("Finance Management", 400);
+        Product p5 = createProduct("Enterprise Resource Planning", 440);
+        productRepository.insert(Arrays.asList(p1, p2, p3, p4, p5));
+
+        mockMvc.perform(get("/products")
+                .param("keyword", "Manage")
+                .param("orderBy", "price")
+                .param("sortRule", "asc")
+                .headers(httpHeaders))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].id").value(p2.getId()))
+                .andExpect(jsonPath("$[1].id").value(p1.getId()))
+                .andExpect(jsonPath("$[2].id").value(p4.getId()))
+                .andExpect(jsonPath("$[3].id").value(p3.getId()));
+    }
+
+    private Product createProduct(String name, int price) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+
+        return product;
     }
 
 }
