fabc49ccb67ed67e04d160039f7c8e5c810c9675
Vincent <eric61011@gmail.com>
2019-07-31 23:25:29 +0800

    Upload chapter 12

---
+++ src/test/java/com/vincent/demo/entity/Publisher.java
@@ -0,0 +1,31 @@
+package com.vincent.demo.entity;
+
+public class Publisher {
+    private String companyName;
+    private String address;
+    private String tel;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
