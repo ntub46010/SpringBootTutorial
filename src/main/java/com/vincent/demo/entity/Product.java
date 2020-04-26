b684db4e40b22468ec086771bf1435bf982fceac
Vincent <eric61011@gmail.com>
2020-04-26 23:01:14 +0800

    Add product price field. Init mock product DB. Refactor returning status code.

--- src/main/java/com/vincent/demo/entity/Product.java
+++ src/main/java/com/vincent/demo/entity/Product.java
@@ -3,6 +3,17 @@ package com.vincent.demo.entity;
 public class Product {
     private String id;
     private String name;
+    private int price;
+
+    public Product() {
+
+    }
+
+    public Product(String id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
     public String getId() {
         return id;
@@ -18,5 +29,13 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
