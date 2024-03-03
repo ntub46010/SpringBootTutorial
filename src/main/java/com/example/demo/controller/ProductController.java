cd577b8014ecc3336d89d2e8c2f1b755a90deeb2
Vincent <eric61011@gmail.com>
2024-03-04 00:34:16 +0800

    Update chapter 4.

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Product;
-import org.springframework.http.HttpStatus;
+import com.example.demo.param.ProductRequestParameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 @RestController
+@RequestMapping(path = "/products")
 public class ProductController {
     private static final Map<String, Product> productMap = new HashMap<>();
 
@@ -23,49 +24,85 @@ public class ProductController {
         ).forEach(p -> productMap.put(p.getId(), p));
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable("id") String productId) {
         var product = productMap.get(productId);
         return product == null
-                ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
-                : ResponseEntity.status(HttpStatus.OK).body(product);
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(product);
     }
 
-    @PostMapping("/products")
+    @PostMapping
     public ResponseEntity<Void> createProduct(@RequestBody Product product) {
         if (product.getId() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.badRequest().build();
         }
 
         if (productMap.containsKey(product.getId())) {
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+            return ResponseEntity.unprocessableEntity().build();
         }
 
         productMap.put(product.getId(), product);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .build(Map.of("id", product.getId()));
+
+        return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Void> updateProduct(
             @PathVariable("id") String productId, @RequestBody Product request) {
         var product = productMap.get(productId);
         if (product == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.notFound().build();
         }
 
         product.setName(request.getName());
         product.setPrice(request.getPrice());
 
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable("id") String productId) {
         if (!productMap.containsKey(productId)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.notFound().build();
         }
 
         productMap.remove(productId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts(
+            @ModelAttribute ProductRequestParameter param
+    ) {
+        var orderField = param.getOrderField();
+        var sortDir = param.getSortDir();
+        var keyword = param.getSearchKey();
+
+        Comparator<Product> comparator;
+        if ("name".equals(orderField)) {
+            comparator = Comparator.comparing(x -> x.getName().toLowerCase());
+        } else if ("price".equals(orderField)) {
+            comparator = Comparator.comparing(Product::getPrice);
+        } else {
+            comparator = (a, b) -> 0;
+        }
+
+        if ("desc".equalsIgnoreCase(sortDir)) {
+            comparator = comparator.reversed();
+        }
+
+        var products = productMap.values()
+                .stream()
+                .filter(x -> x.getName().toLowerCase().contains(keyword.toLowerCase()) ||
+                        x.getId().contains(keyword))
+                .sorted(comparator)
+                .toList();
+        return ResponseEntity.ok(products);
     }
 }
---
+++ src/main/java/com/example/demo/param/ProductRequestParameter.java
@@ -0,0 +1,31 @@
+package com.example.demo.param;
+
+public class ProductRequestParameter {
+    private String searchKey;
+    private String orderField;
+    private String sortDir;
+
+    public String getSearchKey() {
+        return searchKey;
+    }
+
+    public void setSearchKey(String searchKey) {
+        this.searchKey = searchKey;
+    }
+
+    public String getOrderField() {
+        return orderField;
+    }
+
+    public void setOrderField(String orderField) {
+        this.orderField = orderField;
+    }
+
+    public String getSortDir() {
+        return sortDir;
+    }
+
+    public void setSortDir(String sortDir) {
+        this.sortDir = sortDir;
+    }
+}
